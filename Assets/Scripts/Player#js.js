da72ecaf476ae391bf8aa1f7135de8b6880906f3
minimo <minimo.mobile@gmail.com>
2014-05-02 13:07:02 +0900

    Chapter 2 finished. javascrip version.

---
+++ Assets/Scripts/Player#js.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speed = 5;
+
+function Start () {
+
+}
+
+function Update () {
+	var x = Input.GetAxisRaw("Horizontal");
+	var y = Input.GetAxisRaw("Vertical");
+	
+	var direction = Vector2(x,y).normalized;
+	rigidbody2D.velocity = direction * speed;
+}
